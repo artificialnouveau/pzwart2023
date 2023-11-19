98f924f570163cc0e2cbb447efec66572fbf7ca6
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-11-19 13:22:31 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="voiceanalysis",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai-whisper",
+        "soundfile",
+        "simple-diarizer",
+        "pydub",
+        "matplotlib",
+        "seaborn",
+        "spleeter",
+        "jupyterlab",
+        "notebook",
+        "pytube",
+        "moviepy",
+    ],
+    dependency_links=[
+        "git+https://github.com/artificialnouveau/my-voice-analysis/#egg=my-voice-analysis"
+    ],
+)
